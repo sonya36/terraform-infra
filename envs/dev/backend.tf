ca38ce2e341d5b152864d1e2058d983d5b4d7b57
sonya36 <soniyashrm399gmail.com>
2026-02-17 16:24:57 +0545

    edited bucket name to unique

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cloudtech-terraform-state"
+    bucket         = "cloudtech-terraform-state-664418970145"
     key            = "soniya-teams/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
